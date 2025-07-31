5304dfe99c69dc1f016dcfc8738628553f11ec40
Documenter.jl <documenter@juliadocs.github.io>
2025-07-31 12:50:14 +0000

    build based on 9d6d750

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#eeML","page":"Home","title":"eeML","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for eeML.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
